f8ea7816b906949ee7977f43009b95aabad8727a
Okuhle Ntakana <159913477+OkuhleNtakana@users.noreply.github.com>
2025-03-01 20:49:27 +0200

    Add files via upload

--- E-Commerce-Web/src/app/home/home.component.ts
+++ E-Commerce-Web/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import {Router,RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  router = inject(Router);
 }
